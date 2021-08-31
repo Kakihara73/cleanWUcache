e7a510375f9d93cd9b6925c247642563ac2612dd
Kakihara73 <63397338+Kakihara73@users.noreply.github.com>
2021-08-31 10:51:01 +0200

    Make changes suggested by @IvanG76

--- cleanWUcache.cmd
+++ cleanWUcache.cmd
@@ -3,8 +3,7 @@ NET STOP bits
 NET STOP wuauserv
 ping 127.0.0.1 > nul
 IF EXIST %windir%\SoftwareDistribution.OLD RD /S /Q %windir%\SoftwareDistribution.OLD
-CD %windir%
-REN SoftwareDistribution SoftwareDistribution.OLD
+REN %windir%\SoftwareDistribution SoftwareDistribution.OLD
 ping 127.0.0.1 > nul
 NET START wuauserv
 NET START bits
